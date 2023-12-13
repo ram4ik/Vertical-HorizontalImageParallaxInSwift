83b047bc90d8795e2f78430f6574d31b08871727
ram4ik <ram4ik@hotmail.com>
2023-12-13 20:34:31 +0200

    init

--- Vertical&HorizontalImageParallaxInSwift/ContentView.swift
+++ Vertical&HorizontalImageParallaxInSwift/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    var images = ["swift", "apple.logo", "apple.terminal", "applewatch.side.right", "homepod.and.appletv"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 5) {
+                ForEach(images, id: \.self) { item in
+                    GeometryReader { geo in
+                        Image(systemName: item)
+                            .resizable()
+                            .scaledToFit()
+                            .offset(x: calculateOffset(scrollOffset: geo.frame(in: .global).minX))
+                            .frame(width: 200, height: 350)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .scrollTransition(axis: .horizontal) { image, phase in
+                                image.scaleEffect(phase.isIdentity ? 1 : 0.8)
+                            }
+                    }
+                    .frame(width: 200, height: 350)
+                }
+            }
+            .scrollTargetLayout()
         }
-        .padding()
+        .contentMargins(20)
+        .scrollTargetBehavior(.viewAligned)
+    }
+    
+    func calculateOffset(scrollOffset: CGFloat) -> CGFloat {
+        let adjustedOffset = scrollOffset / 5
+        return adjustedOffset
     }
 }
 
