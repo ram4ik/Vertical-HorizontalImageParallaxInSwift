879f2b8740b13c56fae38db751b43839125737d6
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-13 20:17:11 +0200

    Initial Commit

---
+++ Vertical&HorizontalImageParallaxInSwift/Vertical_HorizontalImageParallaxInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vertical_HorizontalImageParallaxInSwiftApp.swift
+//  Vertical&HorizontalImageParallaxInSwift
+//
+//  Created by test on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Vertical_HorizontalImageParallaxInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
